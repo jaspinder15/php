e17667558945a915af11716c9cff9895b6e7157b
jaspinder1995 <jkaur@conestogac.on.ca>
2021-03-01 16:24:26 -0500

    first  push

---
+++ bananna.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class bananna{
+  public static void main(String[]args){
+    System.out.println("what is your name?");
+    Scanner sc = new Scanner(System.in);
+    String userinput;
+    userinput = sc.nextLine();
+    System.out.println("hello tata heyy heyy" + userinput);
+    System.out.println("what is your hobby");
+    userinput = sc.nextLine();
+    System.out.println("oh i love it " + userinput + " too");
+
+  }
+}
